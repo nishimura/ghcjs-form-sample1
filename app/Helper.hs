f269f62e3b302bbffcfad88d8882d2fa4f806108
Satoshi Nishimura <nishim314@gmail.com>
2019-06-11 15:44:52 +0900

    refactoring...

--- app/Helper.hs
+++ app/Helper.hs
@@ -18,9 +18,7 @@ getElementById' :: (MonadJSM m, ToJSString elementId) =>
                    HTMLDocument -> elementId -> m (Maybe HTMLElement)
 getElementById' doc eid = do
   optEl <- getElementById doc eid
-  case optEl of
-    Just el -> castTo HTMLElement el
-    Nothing -> return Nothing
+  maybe (return Nothing) (castTo HTMLElement) optEl
 
 
 getElement :: (Tag ret (HtmlTag ret), IsGObject ret, ToJSString id, Show id, MonadJSM m) =>
@@ -28,6 +26,7 @@ getElement :: (Tag ret (HtmlTag ret), IsGObject ret, ToJSString id, Show id, Mon
 getElement doc eid tag = do
   optEl <- getElementById doc eid
   case optEl of
+    Nothing -> error $ "not found: id [" ++ (show eid) ++ "]"
     Just el -> do
       let t = tagCast tag el
           n = tagName tag
@@ -35,7 +34,6 @@ getElement doc eid tag = do
       if n == tn
         then return t
         else error $ "not found: id [" ++ (show eid) ++ "] tagName [" ++ (tagName tag) ++ "]"
-    Nothing -> error $ "not found: id [" ++ (show eid) ++ "]"
 
 
 data HtmlTag ret where
